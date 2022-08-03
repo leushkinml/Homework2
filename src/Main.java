887927dc7d4a62c04eb51a3eeb4f4346f5947426
leush <leushkinml@mail.ru>
2022-08-03 23:48:34 +0300

    Task3

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,15 @@ public class Main {
         int bananas = 5;
         int bananaWeight = 80;
         int milk = 200;
-        double milkWeight = (105.00 / 100.00);
+        double milkWeightPer1Ml = (105.00 / 100.00);
         int ice_cream = 2;
         int ice_creamWeight = 100;
         int egs = 4;
         int egsWeight = 70;
 
-        double mixedWeightInGramm = (bananas * bananaWeight) + (milk * milkWeight) + (ice_cream * ice_creamWeight) + (egs * egsWeight);
-        double mixedWeightInKg = mixedWeightInGramm / 1000;
+        double mixedWeightInGramm = (bananas * bananaWeight) + (milk * milkWeightPer1Ml) + (ice_cream * ice_creamWeight) + (egs * egsWeight);
+        double grammPerKg = 1000;
+        double mixedWeightInKg = mixedWeightInGramm / grammPerKg;
 
         System.out.println("Вес спорт-завтрака в граммах " + mixedWeightInGramm + ".");
         System.out.println("Вес спорт-завтрака в килограммах " + mixedWeightInKg + ".");
