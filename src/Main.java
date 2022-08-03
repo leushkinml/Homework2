02b070f97e6821b2efe0f8959580d542408798d7
leush <leushkinml@mail.ru>
2022-08-03 22:27:48 +0300

    Task2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 2
+        // 1 вариант
+        double boxerWeight1 = 78.2;
+        double boxerWeight2 = 82.7;
+        double totalBoxersWeight = boxerWeight1 + boxerWeight2;
+        double deltaBoxersWeight = boxerWeight2 - boxerWeight1;
+
+        System.out.println("Общий вес двух бойцов " + totalBoxersWeight + " кг.");
+        System.out.println("Разница между весами бойцов " + deltaBoxersWeight + " кг.");
+
+        // 2 вариант
+
+        double boxWeight1 = 78.2;
+        double boxWeight2 = 82.7;
+
+        System.out.println("Общий вес двух бойцов " + (boxWeight1 + boxWeight2) + " кг.");
+        System.out.println("Разница между весами бойцов " + (boxWeight2 - boxWeight1) + " кг.");
     }
 }
