8aa08a76e67d17c735f88cc6794379b1d7e19115
leush <leushkinml@mail.ru>
2022-08-03 22:14:05 +0300

    Task4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 4
+        int weightToLoose = (7 * 1000);
+        int bigLoosing = 500;
+        int smoleLoosing = 250;
+        int slowlyLoosing = weightToLoose / smoleLoosing;
+        int fastLoosing = weightToLoose / bigLoosing;
+
+        System.out.println("Для похудания по 250 грамм в день понадобится " + slowlyLoosing + " дней.");
+        System.out.println("Для похудания по 500 грамм в день понадобится " + fastLoosing + " дней.");
+        System.out.println("В среднем для похудания понадобится " + ((slowlyLoosing + fastLoosing) / 2)+ " дней.");
     }
 }
