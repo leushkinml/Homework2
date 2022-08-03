1b8720d14b9028429d8bc2e62ad5b3edbe15c542
leush <leushkinml@mail.ru>
2022-08-03 21:36:36 +0300

    Task2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-     // 1 вариант
-        double boxerWeight1 = 78.2;
-        double boxerWeight2 = 82.7;
-        double totalBoxersWeight = boxerWeight1 + boxerWeight2;
-        double deltaBoxersWeight = boxerWeight2 - boxerWeight1;
 
-        System.out.println("Общий вес двух бойцов " + totalBoxersWeight + " кг.");
-        System.out.println("Разница между весами бойцов " + deltaBoxersWeight + " кг.");
+        int bananas = 5;
+        int bananaWeight = 80;
+        int milk = 200;
+        double milkWeight = (105.00 / 100.00);
+        int ice_cream = 2;
+        int ice_creamWeight = 100;
+        int egs = 4;
+        int egsWeight = 70;
 
-     // 2 вариант
+        double mixedWeightInGramm = (bananas * bananaWeight) + (milk * milkWeight) + (ice_cream * ice_creamWeight) + (egs * egsWeight);
+        double mixedWeightInKg = mixedWeightInGramm / 1000;
 
-        double boxWeight1 = 78.2;
-        double boxWeight2 = 82.7;
-
-        System.out.println("Общий вес двух бойцов " + (boxWeight1 + boxWeight2) + " кг.");
-        System.out.println("Разница между весами бойцов " + (boxWeight2 - boxWeight1) + " кг.");
+        System.out.println("Вес спорт-завтрака в граммах " + mixedWeightInGramm + ".");
+        System.out.println("Вес спорт-завтрака в килограммах " + mixedWeightInKg + ".");
 
     }
 }
